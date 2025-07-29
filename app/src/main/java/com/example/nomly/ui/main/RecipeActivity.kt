7f5b2eeaec1ff6ce24e22472fba376028600a223
Acer <yasserashraf3142@gmail.com>
2025-07-29 13:29:25 +0300

    structure

--- app/src/main/java/com/example/nomly/ui/main/RecipeActivity.kt
+++ app/src/main/java/com/example/nomly/ui/main/RecipeActivity.kt
@@ -1,16 +1,17 @@
-package com.example.nomly
+package com.example.nomly.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recipeapp.R
 
-class MainActivity : AppCompatActivity() {
+class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
